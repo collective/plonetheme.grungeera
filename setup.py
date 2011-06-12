f1495a634397192644ecd449cba9583bb3a25dda
Alex Clark <aclark@aclark.net>
2011-06-12 17:37:27 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1.1'
 setup(name='plonetheme.grungeera',
       version=version,
       description="An installable Diazo theme for Plone 4.1",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
